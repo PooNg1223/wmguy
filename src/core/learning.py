94c250dc8d69911ea936a07dcf59596af8da0dcb
PooNg1223 <jaepoong92@gmail.com>
2025-02-16 21:38:34 +0900

    fix: Add Learning class and update configurations

---
+++ src/core/learning.py
@@ -0,0 +1,13 @@
+class Learning:
+    def __init__(self):
+        self.model = None
+        self.training_data = []
+    
+    def train(self, data):
+        self.training_data.extend(data)
+        # Training logic will be implemented here
+        pass
+    
+    def predict(self, input_data):
+        # Prediction logic will be implemented here
+        return None 
